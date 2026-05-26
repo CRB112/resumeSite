73f186c32a843e1dcf16cead5c5ee6a38cccf326
Connor <connorrb1111@gmail.com>
2026-05-26 16:05:00 -0400

    Added frontend components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <h1>Hello, my portfolio starts here</h1>
+    </div>
+  )
+}
+
+export default App
